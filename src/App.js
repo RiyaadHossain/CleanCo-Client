9d49f59dc2a56c8f48cc2fce989d691c0108c848
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-05-16 15:21:08 +0600

    Router-Dom, Tailwind, DaisyUI added. Besides, Router set up and initial navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import Contact from "./Pages/Contact"
+import Login from "./Pages/Login"
+import Service from "./Pages/Service"
 
 
 function App() {
   return (
-    <div>
-     
-    </div>
+    <Navbar>
+      <Routes>
+        <Route path="/"  element={<Home/>}/>
+        <Route path="/about"  element={<About/>}/>
+        <Route path="/contact"  element={<Contact/>}/>
+        <Route path="/login"  element={<Login/>}/>
+        <Route path="/service"  element={<Service/>}/>
+      </Routes>
+    </Navbar>
   );
 }
 
